d15146e00f8ec4e74c7553212c4a2d8ad10bfc91
olivajames110 <olivajames110@gmail.com>
2020-01-24 12:07:35 -0500

    Updates

---
+++ precache-manifest.1b22f6c7b404e5b6180dd59cdd16970a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89bb355280b2d495bcf125be9eb4a37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0d7eddbc8720edd9fe2",
+    "url": "/static/css/main.2194f7d4.chunk.css"
+  },
+  {
+    "revision": "fde5898bef1cee99b8ed",
+    "url": "/static/js/2.899316af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.899316af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0d7eddbc8720edd9fe2",
+    "url": "/static/js/main.88efca1f.chunk.js"
+  },
+  {
+    "revision": "a794f302678390a763bd",
+    "url": "/static/js/runtime-main.dffbfcd7.js"
+  }
+]);
