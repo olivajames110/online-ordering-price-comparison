12a23abf6c3e22d385f04b7b59f619631cb372a2
olivajames110 <olivajames110@gmail.com>
2020-01-24 12:13:30 -0500

    Updates

---
+++ precache-manifest.b0696a1b1bbd7aecca31e5820185c77b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d6687fe1898016bd40bbff8e6604fc",
+    "url": "/online-ordering-price-comparison/index.html"
+  },
+  {
+    "revision": "b1eb3d379e1d2fbf8072",
+    "url": "/online-ordering-price-comparison/static/css/main.2194f7d4.chunk.css"
+  },
+  {
+    "revision": "fde5898bef1cee99b8ed",
+    "url": "/online-ordering-price-comparison/static/js/2.899316af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/online-ordering-price-comparison/static/js/2.899316af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1eb3d379e1d2fbf8072",
+    "url": "/online-ordering-price-comparison/static/js/main.a97c0bb7.chunk.js"
+  },
+  {
+    "revision": "30487d9fd4bf4822f447",
+    "url": "/online-ordering-price-comparison/static/js/runtime-main.2d74de76.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b22f6c7b404e5b6180dd59cdd16970a.js"
+  "/online-ordering-price-comparison/precache-manifest.b0696a1b1bbd7aecca31e5820185c77b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/online-ordering-price-comparison/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
